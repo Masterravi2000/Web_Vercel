c24f9194f2eb4b9993fa6c940fd39a5b7b5b6691
Masterravi2000 <raviqazqwerty123456@gmail.com>
2025-04-28 18:49:19 +0530

    now lets check gaian this time

--- app/components/artickeform/articleform.tsx
+++ app/components/artickeform/articleform.tsx
@@ -206,6 +206,7 @@
              e.currentTarget.style.height = e.currentTarget.scrollHeight + "px";
            }}
          />
+         <div></div>
          <button
            type="submit"
            className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
