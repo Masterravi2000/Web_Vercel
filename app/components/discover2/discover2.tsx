f760c499c04ced0581a3373ad0226ddff740e1fe
Masterravi2000 <raviqazqwerty123456@gmail.com>
2025-05-12 22:35:10 +0530

    Minor fixes 3

--- app/components/discover2/discover2.tsx
+++ app/components/discover2/discover2.tsx
@@ -53,7 +53,7 @@ const SportsCommunitySection: FC<Props> = ({ onDownloadClick }) => {
             {/* // for phones screen */}
             <div className="block md:hidden px-4">
                 <h1
-                    className="text-[1.6rem] font-[700] text-[#E7E7E7 font-black text-center leading-[2.5rem]"
+                    className="text-[1.8rem] font-[900] text-[#E7E7E7] font-black text-center leading-[2.5rem]"
                     data-delay="1000"
                     data-aos="fade-up"
                 >
